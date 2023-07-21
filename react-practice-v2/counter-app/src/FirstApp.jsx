08fe8fdaf490cd04e9d98e99bd0c9543186c0030
ianklebold <ianstgo@gmail.com>
2023-07-21 14:12:57 -0300

    REACT PRACTICE V2 - 001 - Primer componente

---
+++ react-practice-v2/counter-app/src/FirstApp.jsx
@@ -0,0 +1,6 @@
+//Punto de entrada de la aplicacion
+//Los nombres de los functional components se escriben con UpperCamelCase!
+export const FirstApp = () => {
+    return ( <h1>Hola mundo</h1> );
+};
+
--- react-practice-v2/counter-app/src/main.jsx
+++ react-practice-v2/counter-app/src/main.jsx
@@ -2,18 +2,12 @@
 import React from 'react'
 //Herramienta para renderizar react
 import ReactDOM from 'react-dom/client'
+import {FirstApp} from './FirstApp';
 
-//Punto de entrada de la aplicacion
-//Los nombres de los functional components se escriben con UpperCamelCase!
-function App() {
-    // documento.createElement... es lo mismo:
-    return <h1>Hola mundo</h1>;
-
-}
 
 //Tomamos la etiqueta de html identificada con root y le renderizamos el functional components
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>
 );
