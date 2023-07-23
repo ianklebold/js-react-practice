62cc5293f1bfb7402e90dccc96d18126c347ab2a
ianklebold <ianstgo@gmail.com>
2023-07-23 00:26:38 -0300

    REACT PRACTICE V2 - 002 - Agrupando elementos html

--- react-practice-v2/counter-app/src/FirstApp.jsx
+++ react-practice-v2/counter-app/src/FirstApp.jsx
@@ -1,6 +1,13 @@
 //Punto de entrada de la aplicacion
 //Los nombres de los functional components se escriben con UpperCamelCase!
+// <> Es un fragmento, agrupa varias etiquetas de html. <> Es un nodo padre.
 export const FirstApp = () => {
-    return ( <h1>Hola mundo</h1> );
+    return ( 
+    <>
+        <h1>Hola mundo</h1> 
+        <p>Este es un subtitulo</p>
+    </>
+    
+    );
 };
 
--- react-practice-v2/counter-app/src/main.jsx
+++ react-practice-v2/counter-app/src/main.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 //Herramienta para renderizar react
 import ReactDOM from 'react-dom/client'
 import {FirstApp} from './FirstApp';
-
+import {HelloWorldApp} from './HelloWorldApp';
 
 //Tomamos la etiqueta de html identificada con root y le renderizamos el functional components
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <FirstApp />
+        <HelloWorldApp />
     </React.StrictMode>
 );
