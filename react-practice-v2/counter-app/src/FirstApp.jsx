ccb4b0d9630d69776c4d77697981e702c1488d82
ianklebold <ianstgo@gmail.com>
2023-07-23 01:18:35 -0300

    REACT PRACTICE V2 - 005 - Props y PropTypes

--- react-practice-v2/counter-app/src/FirstApp.jsx
+++ react-practice-v2/counter-app/src/FirstApp.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 //Punto de entrada de la aplicacion
 //Los nombres de los functional components se escriben con UpperCamelCase!
 // <> Es un fragmento, agrupa varias etiquetas de html. <> Es un nodo padre.
@@ -11,14 +13,22 @@ const myNameIs = () => {
     return "Ian";
 }
 
-export const FirstApp = () => {
+
+//Los parametros que recibe el componente se llaman props (Objeto). Siempre va encerrado con {}
+//Las props son informacion que fluye del padre (Main) al Hijo (First App)
+export const FirstApp = ({title, subtitle}) => {
     return ( 
     <>
-        <h1>Hola mundo</h1> 
+        <h1> {`Hola mundo este es un titulo : ${title}`} </h1> 
         <p>{ `${newMessage} y mi nombre es ${myNameIs()}` }</p>
-        <p>Este es un subtitulo</p>
+        <p>{`Este es un subtitulo : ${subtitle}`}</p>
     </>
     
     );
 };
 
+//Con el Proptypes podemos definirle un tipo a las props
+FirstApp.propTypes = {
+    title : PropTypes.string.isRequired,
+    subtitle : PropTypes.number
+}
--- react-practice-v2/counter-app/src/main.jsx
+++ react-practice-v2/counter-app/src/main.jsx
@@ -10,9 +10,10 @@ import './styles.css';
 
 
 //Tomamos la etiqueta de html identificada con root y le renderizamos el functional components
+//Podemos pasarle props al componente hijo. Cuando usamos {} es para pasar numeros
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp title = "Nuevo componente" subtitle = {123}/>
         <HelloWorldApp />
     </React.StrictMode>
 );
