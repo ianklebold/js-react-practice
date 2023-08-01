006f47c1fa77b96299522020beea184e11d842f3
ianklebold <ianstgo@gmail.com>
2023-07-31 21:55:46 -0300

    Usando toBe e importando intellisense para JEST

--- react-practice-v2/counter-app/test/demo.test.js
+++ react-practice-v2/counter-app/test/demo.test.js
@@ -1,3 +1,24 @@
-test('Titulo de la prueba', () => { 
+//npm add -D @types/jest para el intellisense de JEST
+//Usualmente no se escribe mas de un describe por archivo test, pero si se puede hacer
+
+
+describe('Pruebas primarias', () => { 
+//Agrupador de test
+    test('Titulo de la prueba', () => { 
+    
+    });
+   
+   test('Esta prueba no debe fallar', () => { 
+       
+       //1. Inicializacion
+       const message1 = 'Hola mundo';
+   
+       //2. Estimulo
+       const message2 = message1.trim();
+   
+       //3. Obsevar el comportamiento... esperado
+       expect( message1 ).toBe( message2 ); //Oberservar message1 debe ser igual a message2
+       
+    });
     
  });
